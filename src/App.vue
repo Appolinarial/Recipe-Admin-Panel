a8ae7699345ebd4fe4bf6981a17bcf087a4feeb7
Paulina <87566112+Appolinarial@users.noreply.github.com>
2025-03-25 22:28:14 +0300

    Added App.vue

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup>
+import { RouterView } from 'vue-router'
+</script>
+
+<template>
+  <RouterView />
+</template>
+
+<style lang="sass" scoped>
+@use "./assets/styles/index" as c;
+</style>
