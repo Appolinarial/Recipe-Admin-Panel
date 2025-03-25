8be222eb931c1d9afc31695546421a02dcea4637
Paulina <87566112+Appolinarial@users.noreply.github.com>
2025-03-25 22:35:25 +0300

    Added Home.vue

---
+++ src/pages/Home.vue
@@ -0,0 +1,13 @@
+cript setup>
+import AppLayout from '@/layouts/AppLayout.vue';
+import AppButton from '@/components/AppButton.vue';
+</script>
+
+<template>
+    <AppLayout>
+        <template #title>Рецепты</template>
+        <template #controls>
+            <AppButton text="Добавить рецепт"/>
+        </template>
+    </AppLayout>
+</template>
