2f5f443101eb04fe0c39661d24f43755fb5c65e7
Paulina <87566112+Appolinarial@users.noreply.github.com>
2025-03-25 22:33:05 +0300

    Added AppButton.vue

---
+++ src/components/AppButton.vue
@@ -0,0 +1,25 @@
+<script setup>
+const props = defineProps({
+    text: {
+        type: String,
+        default: '',
+    },
+});
+</script>
+
+<template>
+    <el-button type="primary" class="btn">
+        {{ text }}
+    </el-button>
+
+</template>
+
+<style lang="sass" scoped>
+@use "../assets/styles/index" as area;
+
+.btn 
+    background-color: #4d0690
+    font-size: 17px
+    padding: 20px 24px
+    border-radius: 4px
+</style>
