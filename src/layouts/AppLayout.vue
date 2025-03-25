b6609f0eaeed8623bd9c17a8609843a7ab4823ce
Paulina <87566112+Appolinarial@users.noreply.github.com>
2025-03-25 22:34:07 +0300

    Added AppLayout.vue

---
+++ src/layouts/AppLayout.vue
@@ -0,0 +1,45 @@
+<script setup>
+import AppMenu from '@/components/AppMenu.vue';
+</script>
+
+<template>
+    <div class="root">
+        <div class="menuInner">
+            <AppMenu />
+        </div>
+        <main class="main">
+            <header class="header">
+                <div class="title">
+                    <slot name="title"></slot>
+                </div>
+                <div class="controls">
+                    <slot name="controls"></slot>
+                </div>
+            </header>
+        </main>
+    </div>
+</template>
+
+<style lang="sass" scoped>
+@use "../assets/styles/index" as area;
+
+.root     
+    display: flex
+
+.menuInner
+    width: 70px
+
+.main
+    flex: 1
+    padding: 20px 16px
+
+.header 
+    display: flex
+    justify-content: space-between
+    align-items: center
+
+.title
+    color: #4d0690
+    font-size: 28px
+    font-weight: 700
+</style>
