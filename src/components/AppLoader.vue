61e845838dc45485c2b4f9de4b5b5c139a308ae4
Paulina <87566112+Appolinarial@users.noreply.github.com>
2025-03-28 15:27:06 +0300

    Added AppLoader.vue

---
+++ src/components/AppLoader.vue
@@ -0,0 +1,29 @@
+<template>
+    <Transition appear>
+        <div class="root">
+            <div class="heading">
+                Loading...
+            </div>
+        </div>
+    </Transition>
+</template>
+
+<style lang="sass" scoped>
+@use "../assets/styles/index" as area;
+
+.root 
+    z-index: 2
+    position: fixed
+    display: flex
+    justify-content: center
+    align-items: center
+    top:0
+    bottom: 0
+    left: 0
+    right: 0
+    background-color: rgba(#fff, 0.8)
+
+.heading
+    font-size: 28px
+    font-weight: 700px
+</style>
