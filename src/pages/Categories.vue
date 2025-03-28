aa93533923c459fb23857a178aaa35a8dc458ed5
Paulina <87566112+Appolinarial@users.noreply.github.com>
2025-03-28 15:41:13 +0300

    Added page with categories

---
+++ src/pages/Categories.vue
@@ -0,0 +1,24 @@
+<script setup>
+import { ref, computed } from 'vue'
+import { useRootStore } from '@/stores/root';
+import AppLayout from '@/layouts/AppLayout.vue';
+import AppButton from '@/components/AppButton.vue';
+
+
+const rootStore = useRootStore()
+const categories = computed(() => rootStore.categories)
+</script>
+
+<template>
+    <AppLayout>
+        <template #title>Categories</template>
+        <template #controls>
+            <AppButton text="Add Category"/>
+        </template>
+        <template #inner>  
+            <el-table :data="categories" style="width: 100%;">
+                <el-table-column prop="strCategory" label="Category"/>
+            </el-table>
+        </template>
+    </AppLayout>
+</template>
