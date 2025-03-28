7a6d28749952bb554dc129af70a45527297198dc
Paulina <87566112+Appolinarial@users.noreply.github.com>
2025-03-28 15:29:14 +0300

    Added circle-button in  AppButton.vue

--- src/components/AppButton.vue
+++ src/components/AppButton.vue
@@ -4,22 +4,34 @@ const props = defineProps({
         type: String,
         default: '',
     },
+    circle: {
+        type: Boolean,
+        default: false,
+    },
+    icon: {
+        type: String,
+        default: '',
+    },
 });
 </script>
 
 <template>
-    <el-button type="primary" class="btn">
+    <el-button v-if="!circle" type="primary" class="btn">
         {{ text }}
     </el-button>
-
+    <el-button v-else :icon="icon" type="primary" circle class="circle"/>
 </template>
 
 <style lang="sass" scoped>
-@use "../assets/styles/index" as area;
+@use "../assets/styles/index" as area
 
 .btn 
     background-color: #4d0690
     font-size: 17px
     padding: 20px 24px
     border-radius: 4px
+
+.circle
+    background-color: #4d0690
+    font-size: 16px 
 </style>
